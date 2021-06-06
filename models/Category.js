cc90d9ec2fbe56abb0f35461b89a155560aa57e2
brownnicholasj <brown.nicholasj@gmail.com>
2021-06-05 19:59:21 -0500

    updated connection information and setup seeds requirements so seed works appropriately

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		category_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'category',
+	}
 );
 
 module.exports = Category;
